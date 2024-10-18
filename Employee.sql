17d5d748d9817e75e205da048dfa66df877d1e30
khuneninad <ninadkhune@gmail.com>
2024-10-18 17:21:51 +0530

    new commit

---
+++ Employee.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE payroll_system;
+
+USE payroll_system;
+
+CREATE TABLE employees
+(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  Name VARCHAR(100) NOT NULL,
+  phone VARCHAR(15) NOT NULL,
+  email VARCHAR(100) NOT NULL UNIQUE,
+  password VARCHAR(100) NOT NULL,
+  dob DATE NOT NULL
+);
+
